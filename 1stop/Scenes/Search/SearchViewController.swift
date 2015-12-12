1a5fe4922a638c8ce6c693b6d871355808c80c6c
Lei Mingyu <uygnim.iel@gmail.com>
2015-12-12 19:33:35 +0800

    fix project structure

--- 1stop/Scenes/Search/SearchViewController.swift
+++ 1stop/Scenes/Search/SearchViewController.swift
@@ -13,8 +13,6 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
