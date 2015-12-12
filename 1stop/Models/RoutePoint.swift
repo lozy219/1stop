d30a00f26c6b496a36798beb8af030c17017dbe9
Pan Long <aga.panlong@gmail.com>
2015-12-12 22:59:16 +0800

    Add .DS_Store to git ignore file.

---
+++ 1stop/Models/RoutePoint.swift
@@ -0,0 +1,13 @@
+//
+//  RoutePoint.swift
+//  1stop
+//
+//  Created by Long Pan on 12/12/15.
+//  Copyright © 2015 golearn. All rights reserved.
+//
+
+import Cocoa
+
+class RoutePoint: NSObject {
+
+}
