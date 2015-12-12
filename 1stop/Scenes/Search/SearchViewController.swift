e238dec2c082d8eb67edc2e89d0dd343f6b69e22
Lei Mingyu <uygnim.iel@gmail.com>
2015-12-12 20:16:08 +0800

    remove search button

--- 1stop/Scenes/Search/SearchViewController.swift
+++ 1stop/Scenes/Search/SearchViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor.grayColor().CGColor
+        border.frame = CGRect(x: 0, y: self.textField.frame.size.height - width,
+            width: UIScreen.mainScreen().bounds.width, height: self.textField.frame.size.height)
+        
+        border.borderWidth = width
+        self.textField.layer.addSublayer(border)
+        self.textField.layer.masksToBounds = true
     }
     
     override func didReceiveMemoryWarning() {
