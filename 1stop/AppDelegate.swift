2e84143fc710de139fbeb1845dd7356feffce5b9
Lei Mingyu <uygnim.iel@gmail.com>
2015-12-13 03:08:13 +0800

    load content when the app is launched

--- 1stop/AppDelegate.swift
+++ 1stop/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Store.sharedInstance
         return true
     }
 
